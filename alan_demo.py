df54ad0dcd812c00b6b7f00a01b3883f492e68e6
Jackie <email@example.com>
2024-06-26 01:03:49 +0800

    s

--- alan_demo.py
+++ alan_demo.py
@@ -7,9 +7,7 @@ from prefect.deployments import run_deployment
 @flow
 def print_remote_control():
     # Emoji for remote control
-    emoji = "??????"
-    # Print the message with the emoji
-    print(f"Remote Control")
+    print(1)
 
 
 
@@ -18,8 +16,8 @@ def demo():
     run_deployment(name="print_remote_control/print_remote_control_local", as_subflow=True)
 
 if __name__ == "__main__":
-    #source = "https://github.com/yunhzou/Prefect_demo.git"
-    #entrypoint = "alan_demo.py:print_remote_control"
-    #flow.from_source(source=source, entrypoint=entrypoint).deploy(name="print_remote_control_local", work_pool_name="Jackie Computer")
-    #flow.from_source(source=source, entrypoint=entrypoint).deploy(name="print_remote_control_virtual", work_pool_name="Test_WorkPool")
-    demo()
+    source = "https://github.com/yunhzou/Prefect_demo.git"
+    entrypoint = "alan_demo.py:print_remote_control"
+    flow.from_source(source=source, entrypoint=entrypoint).deploy(name="print_remote_control_local", work_pool_name="Jackie Computer")
+    flow.from_source(source=source, entrypoint=entrypoint).deploy(name="print_remote_control_virtual", work_pool_name="Test_WorkPool")
+    #demo()
